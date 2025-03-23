2d794d5415011d7e4fea9054e84e6535fbddfe16
Rapudi <112486977+hlakudi200@users.noreply.github.com>
2025-03-23 21:23:05 +0200

    Second Initial Commit
    This is all project files with the sln

---
+++ RideSharing/Models/Passenger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideSharing.Models
+{
+    public class Passenger:User
+    {
+        public decimal WalletBalance { get; set; }
+        public List<Ride> RideHistory { get; set; } = new List<Ride>();
+
+    }
+}
---
+++ RideSharing/Services/RideService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RideSharing.Models;
+
+namespace RideSharing.Services
+{
+    public class RideService
+    {
+        public List<Ride> AvailableRides { get; set; } = new List<Ride>();
+        public List<Driver> Drivers { get; set; } = new List<Driver>();
+
+        public void RequestRide(Passenger passenger, string pickupLocation, string dropoffLocation)
+        {
+            var availableDrivers = Drivers.Where(d => d.AvailabilityStatus).ToList();
+
+            if (availableDrivers.Any())
+            {
+                var ride = new Ride
+                {
+                    RideId = AvailableRides.Count + 1,
+                    Passenger = passenger,
+                    PickupLocation = pickupLocation,
+                    DropoffLocation = dropoffLocation,
+                    Fare = CalculateFare(pickupLocation, dropoffLocation),
+                    Status = "Pending"
+                };
+                AvailableRides.Add(ride);
+                Console.WriteLine("Ride requested successfully. Waiting for a driver to accept...");
+            }
+            else
+            {
+                throw new Exception("No available drivers.");
+            }
+        }
+
+        private decimal CalculateFare(string pickupLocation, string dropoffLocation)
+        {
+          //simulating price coz there is no actual distance
+            Random random = new Random();
+            return random.Next(10,500);
+        
+        }
+    }
+}
